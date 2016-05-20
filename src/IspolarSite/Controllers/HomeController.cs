c67dd71ef5b34f3a3508bc35c400f2a002bbc4d1
mannsi <mannsi1983@gmail.com>
2016-05-20 13:41:29 +0000

    Draft 3 of site

--- src/IspolarSite/Controllers/HomeController.cs
+++ src/IspolarSite/Controllers/HomeController.cs
@@ -8,22 +8,20 @@ namespace IspolarSite.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("About")]
         public IActionResult About()
         {
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
-        public IActionResult Error()
+        [Route("Products")]
+        public IActionResult Products()
         {
             return View();
         }
